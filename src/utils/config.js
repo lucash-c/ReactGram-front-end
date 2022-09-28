08230abf0fe8b16211b8e80930599ea793005093
Lucas Campos <lucash_c@yahoo.com.br>
2022-09-27 22:02:55 -0300

    preparando para deploy

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
-export const api = "http://localhost:5000/api";
-export const uploads = "http://localhost:5000/uploads";
+export const api = process.env.REACT_APP_API;
+export const uploads = process.env.REACT_APP_API_UPLOADS;
 
 export const requestConfig = (method, data, token = null, image = null) => {
 
